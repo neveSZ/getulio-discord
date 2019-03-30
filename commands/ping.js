e3d58f34e03b8ee466d9c4d5d9c13ec6cca32898
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-30 19:45:25 -0300

    Criado comando ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,8 @@
 // Devolve o ping do bot ao servidor
+module.exports = {
+  run: async (client, message, args) => {
+    await message.channel.send(
+        `Pong! ${new Date().getTime() - message.createdTimestamp} ms`);
+  },
+  help: {name: 'ping', description: 'Mostra a latencia do BOT', usage: 'ping'}
+}
