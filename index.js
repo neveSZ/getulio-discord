e3d0d170396be429ac4698f3fac2ee6eb0466b31
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-30 19:44:11 -0300

    Criado index

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const {readdirSync} = require('fs');
+const Enmap = require('enmap');
+const client = new Discord.Client();
+client.commands = new Enmap();
+const cmdFiles = readdirSync('./commands/');
+
+// Carregar comandos
+cmdFiles.forEach(f => {
+  try {
+    const props = require(`./commands/${f}`);
+    if (f.split('.').slice(-1)[0] !== 'js') return;
+
+    console.log('[LOG]', `Carregando o comando: ${props.help.name}`);
+    if (props.init) {
+      props.init(client);
+    }
+    client.commands.set(props.help.name, props);
+  } catch (e) {
+    console.log('[LOG]', `Impossivel carregar o comando ${f}: ${e}`);
+  }
+})
+
+// Carregar eventos
+const evtFiles = readdirSync('./events/')
+console.log('[LOG]', `Carregando ${evtFiles.length} eventos`);
+evtFiles.forEach(f => {
+  const eventName = f.split('.')[0];
+  const event = require(`./events/${f}`);
+  client.on(eventName, event.bind(null, client));
+})
+
+client.on('[ERRO]', (err) => {console.log('[ERRO]', err)});
+
+client.login(process.env.AUTH_TOKEN);
