7879ccb20bf0d5a9c9a4ce8eee9269cb94623a6b
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-31 11:43:47 -0300

    Comando entrar

--- commands/entrar.js
+++ commands/entrar.js
@@ -1,1 +1,33 @@
 // Entrar no canal de voz do usuario
+module.exports = {
+  run: async (client, message, args) => {
+    const voiceChannel = message.member.voiceChannel;
+    // Verificar se o membro esta em um canal de voz
+    if (!voiceChannel)
+      return message.channel.send(`${
+          message
+              .author}\nVocê precisa estar em um canal de voz para usar este comando`);
+
+    // Verificar permissoes
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has('CONNECT')) {
+      return message.channel.send(
+          `${message.author}\nNão tenho permissão para entrar neste canal`);
+    }
+    if (!permissions.has('SPEAK')) {
+      return message.channel.send(`${message.author}\n${
+          message.author}\nNão tenho permissão para falar neste canal`);
+    }
+
+    voiceChannel.join()
+        .then(
+            connection => console.log(
+                '[LOG]', `Conectado no canal #${voiceChannel.name}`))
+        .catch(console.error('[ERROR]', 'Ocorreu um erro ao entrar no canal'));
+  },
+  help: {
+    name: 'entrar',
+    description: 'Entrar no canal de voz do usuario solicitante',
+    usage: 'entrar'
+  }
+}
