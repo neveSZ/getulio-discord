fbafc749fa9795cfd3cf716a0aedcc9e35a7ea04
VIctor Neves Silva <38274108+neveSZ@users.noreply.github.com>
2019-03-31 04:04:11 -0300

    Adicionado autor do comando

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,9 @@
 // Devolve o ping do bot ao servidor
 module.exports = {
-  run: async (client, message, args) => {
-    await message.channel.send(
-        `Pong! ${new Date().getTime() - message.createdTimestamp} ms`);
+  run: async (client, message) => {
+    await message.channel.send(`Pong! ${
+        new Date().getTime() -
+        message.createdTimestamp} ms\n${message.author}`);
   },
   help: {name: 'ping', description: 'Mostra a latencia do BOT', usage: 'ping'}
 }
