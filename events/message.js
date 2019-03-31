143a038b6c5162f455037c251fa9b104af8a6234
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-31 11:43:37 -0300

    Resposta para mensagens no privado

--- events/message.js
+++ events/message.js
@@ -1,9 +1,10 @@
 module.exports = async (client, message) => {
-  // Verificar se foi mensagem privada
-  if (!message.guild) return;
-
   // Verificar se nao eh bot
   if (message.author.bot) return;
+
+  // Verificar se foi mensagem privada
+  if (!message.guild)
+    return message.channel.send('Ainda não funciono no privado');
 
   // Verificar se possui o prefixo
   if (message.content.indexOf(process.env.PREFIX) !== 0) return;
@@ -16,10 +17,9 @@ module.exports = async (client, message) => {
 
   // Verificar se o comando esta disponivel
   if (!cmd) {
-    await message.channel.send(
+    return await message.channel.send(
         `${message.author}\nComando não disponível. Digite ${
             process.env.PREFIX}ajuda para ver a lista de comandos.`);
-    return;
   };
 
   console.log(
