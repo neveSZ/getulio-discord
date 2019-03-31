f357af0841a28cfbddc78fd7166d59eacf0dd263
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-31 04:09:41 -0300

    Adicionado comando convite

--- commands/convite.js
+++ commands/convite.js
@@ -1,1 +1,13 @@
 // Devolve um link para adicionar esse bot no discord
+module.exports = {
+  run: async (client, message) => {
+    await message.channel.send(
+        `https://discordapp.com/api/oauth2/authorize?client_id=561628351840649216&permissions=36928576&scope=bot\n${
+            message.author}`);
+  },
+  help: {
+    name: 'convite',
+    description: 'Link para adicionar o bot',
+    usage: 'convite'
+  }
+}
