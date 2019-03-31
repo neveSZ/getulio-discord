ee99d4184739ba80a49cad2a068d6364e652997a
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-31 04:11:49 -0300

    Desabilitado embed link

--- commands/convite.js
+++ commands/convite.js
@@ -2,7 +2,7 @@
 module.exports = {
   run: async (client, message) => {
     await message.channel.send(
-        `https://discordapp.com/api/oauth2/authorize?client_id=561628351840649216&permissions=36928576&scope=bot\n${
+        `<https://discordapp.com/api/oauth2/authorize?client_id=561628351840649216&permissions=36928576&scope=bot>\n${
             message.author}`);
   },
   help: {
