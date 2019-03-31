50bea16dc3660a3dc561c9e27932912e0179bdef
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-31 17:00:42 -0300

    Verificacao se bot esta em algum canal de voz

--- commands/sair.js
+++ commands/sair.js
@@ -8,11 +8,13 @@ module.exports = {
           message
               .author}\nVocê precisa estar em um canal de voz para usar este comando`);
 
+    //Verificar se o bot esta em um canal
+    if (!message.guild.voiceConnection)
+      return message.channel.send(`${message.author}\nNo momento não estou em um canal de voz`);
+
     // Verificar se estao no mesmo canal de voz
     if (voiceChannel.id !== message.guild.voiceConnection.channel.id)
-      return message.channel.send(`${
-          message
-              .author}\nVocê precisa estar no mesmo canal do bot para usar este comando`);
+      return message.channel.send(`${message.author}\nVocê precisa estar no mesmo canal do bot para usar este comando`);
     voiceChannel.leave();
     console.log('[LOG]', `Saiu do canal #${voiceChannel.name}`);
   },
