4d1bf393899f17477dfa7d1d2b1555559188f4ea
Victor Neves Silva <vnsilva2000@gmail.com>
2019-04-04 08:41:17 -0300

    Adicionado a funcao parar

--- commands/parar.js
+++ commands/parar.js
@@ -1,1 +1,14 @@
 // Para a musica atual e remove todas as musicas da lista
+async function cmdParar(message) {
+    const serverQueue = global.queue.get(message.guild.id);
+    const voiceChannel = message.member.voiceChannel;
+
+    // Verificar se o membro esta em um canal de voz
+    if (!voiceChannel)
+        return message.channel.send(`${message.author}\nVocê precisa estar em um canal de voz para usar este comando`);
+    if (!serverQueue)
+        return message.channel.send('Não estou tocando nada');
+    serverQueue.connection.dispatcher.end();
+}
+
+module.exports = cmdParar;
