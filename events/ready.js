80caaee03535199556e7935c3135499ef3442ae2
Victor Neves Silva <vnsilva2000@gmail.com>
2019-04-09 09:04:58 -0300

    Mudanca de status periodica

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,34 @@
 // Acionado toda vez que o bot conecta no discord
+const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
+const listStatus = [{
+    activity: 'conhecimento tácito',
+    type: 'STREAMING'
+}, {
+    activity: 'sessão da tarde',
+    type: 'WATCHING'
+}, {
+    activity: 'tênis em NY',
+    type: 'PLAYING'
+}, {
+    activity: 'pessoas obsoletas',
+    type: 'LISTENING'
+}, {
+    activity: 'no berço esplêndido',
+    type: 'PLAYING'
+}];
+
 async function onReady(client) {
     console.log(`O Bot foi iniciado, atualmente estou em ${client.guilds.size} servidor(es), auxiliando ${client.users.size} usuarios`);
     // Status do bot
-    client.user.setActivity('conhecimento tácito', {
-        type: 'STREAMING',
-        url: 'https://www.twitch.tv/neveSZcs'
-    });
+    while (true) {
+        const i = Math.floor((Math.random() * 5));
+        await client.user.setActivity(listStatus[i].activity, {
+            type: listStatus[i].type,
+            url: 'https://www.twitch.tv/neveSZcs'
+        });
+        console.log(`Alterada a atividade para : ${listStatus[i].activity}`);
+        await timeout(30000); // esperar 30 segundos
+    }
 }
+
 module.exports = onReady;
